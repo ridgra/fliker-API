2d78f4379a6c8e2cfb6b6ed4a35e1f04ce6f7cbe
Ridho Anugrah <im.ridgra@gmail.com>
2021-02-11 11:35:42 +0700

    added server config file

--- config/index.js
+++ config/index.js
@@ -4,4 +4,7 @@ module.exports = {
     dev: process.env.NODE_ENV === 'development',
     prod: process.env.NODE_ENV === 'production',
   },
+  api: {
+    flickrURL: 'https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1',
+  },
 };
